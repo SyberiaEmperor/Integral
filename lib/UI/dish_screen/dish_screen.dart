635d9e6f6a09eeb3430efed4cee2b935008d02a2
Sakorni <ssakorni@gmail.com>
2020-11-12 18:00:24 +0300

    Finished test

--- lib/UI/dish_screen/dish_screen.dart
+++ lib/UI/dish_screen/dish_screen.dart
@@ -22,53 +22,78 @@ class DishScreen extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Center(
-                child: Text(
-                  _dish.name,
-                  style: TextStyle(fontSize: 21),
-                ),
-              ),
-              SizedBox(height: 20.height),
-              Container(
-                width: double.infinity,
-                height: 281.height,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10),
-                    ),
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: CachedNetworkImageProvider(
-                        _dish.url,
-                      ),
-                    )),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.blueGrey[100]),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(10),
-                    bottomRight: Radius.circular(10),
+          child: Stack(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Text(
+                    _dish.name,
+                    style: TextStyle(fontSize: 21),
                   ),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Цена", style: TextStyle(fontSize: 15)),
-                    Text(
-                      "${_dish.price} руб.",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ],
+                SizedBox(height: 20.height),
+                Container(
+                  width: double.infinity,
+                  height: 281.height,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: CachedNetworkImageProvider(
+                          _dish.url,
+                        ),
+                      )),
                 ),
-              )
-            ],
-          ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueGrey[100], width: 0),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Цена", style: TextStyle(fontSize: 15)),
+                      Text(
+                        "${_dish.price} руб.",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Описание:\n\n${_dish.description}",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 15),
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RawMaterialButton(
+                    onPressed: null,
+                    child: Text("Левая хуйня"),
+                  ),
+                  RawMaterialButton(
+                    onPressed: null,
+                    child: Text("Правая хуйня"),
+                    fillColor: Colors.blue,
+                  ),
+                ],
+              ),
+            )
+          ]),
         ));
   }
 }
