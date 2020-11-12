635b65fea106002927f9440b5ad10eec6c389497
Sakorni <ssakorni@gmail.com>
2020-11-12 17:52:00 +0300

    Added picture

--- lib/UI/dish_screen/dish_screen.dart
+++ lib/UI/dish_screen/dish_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:integral/models/dish.dart';
+import 'package:integral/services/responsive_size.dart';
 
 class DishScreen extends StatelessWidget {
   final Dish _dish;
@@ -8,17 +10,65 @@ class DishScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Theme.of(context).canvasColor,
-        actions: [
-          RawMaterialButton(
-            onPressed: null,
-            child: Icon(Icons.shopping_basket),
-          )
-        ],
-      ),
-      body: Center(child: Text("Yup")),
-    );
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).canvasColor,
+          actions: [
+            RawMaterialButton(
+              onPressed: null,
+              child: Icon(Icons.shopping_basket),
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Text(
+                  _dish.name,
+                  style: TextStyle(fontSize: 21),
+                ),
+              ),
+              SizedBox(height: 20.height),
+              Container(
+                width: double.infinity,
+                height: 281.height,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: CachedNetworkImageProvider(
+                        _dish.url,
+                      ),
+                    )),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueGrey[100]),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Цена", style: TextStyle(fontSize: 15)),
+                    Text(
+                      "${_dish.price} руб.",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ResponsiveSize.init(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width);
     return MaterialApp(
-      title: 'Integral',
-      theme: ThemeData(
-        canvasColor: Colors.white,
-        primaryColor: Color(0xffF5F7FA),
-        backgroundColor: Color(0xffF5F7FA),
-        accentColor: Color(0xffFA7022),
-      ),
-      home: DishScreen(Dish.testDish()),
-      //MainPage(),
-    );
+        title: 'Integral',
+        theme: ThemeData(
+          canvasColor: Colors.white,
+          primaryColor: Color(0xffF5F7FA),
+          backgroundColor: Color(0xffF5F7FA),
+          accentColor: Color(0xffFA7022),
+        ),
+        home: Builder(
+          builder: (context) {
+            ResponsiveSize.init(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width);
+            return DishScreen(Dish.testDish());
+          },
+        )
+        //MainPage(),
+        );
   }
 }
