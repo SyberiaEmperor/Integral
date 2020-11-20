1b766d71f8712b53a4f3e31315ac087fbf6010a4
Sakorni <ssakorni@gmail.com>
2020-11-20 16:41:29 +0300

    Added tags

--- lib/UI/dish_screen/dish_screen.dart
+++ lib/UI/dish_screen/dish_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:integral/UI/dish_screen/widgets/tag_field.dart';
 import 'package:integral/models/dish.dart';
 import 'package:integral/services/responsive_size.dart';
 
@@ -13,6 +14,7 @@ class DishScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
+        iconTheme: Theme.of(context).iconTheme,
         backgroundColor: Theme.of(context).canvasColor,
         actions: [
           RawMaterialButton(
@@ -35,69 +37,101 @@ class DishScreen extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 20.height),
-                  Container(
-                    width: double.infinity,
-                    height: 281.height,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
-                      image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image: CachedNetworkImageProvider(
-                          _dish.url,
-                        ),
-                      ),
-                    ),
+                  _PictureAndPrice(
+                    dishPrice: _dish.price,
+                    picUrl: _dish.url,
                   ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueGrey[100], width: 0),
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Цена", style: TextStyle(fontSize: 15)),
-                        Text(
-                          "${_dish.price} руб.",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 20.height),
                   Text(
                     "Описание:\n\n${_dish.description}",
-                    softWrap: true,
                     style: TextStyle(fontSize: 15),
-                  )
+                  ),
+                  SizedBox(height: 20.height),
+                  Wrap(
+                    children: _dish.categories.map((e) => TagField(e)).toList(),
+                  ),
                 ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RawMaterialButton(
-                  onPressed: null,
-                  child: Text("Левая хуйня"),
-                ),
-                RawMaterialButton(
-                  onPressed: null,
-                  child: Text("Правая хуйня"),
-                  fillColor: Colors.blue,
-                ),
-              ],
-            )
+            _ButtonBar(
+              leftFieldCallback: () => print("left"),
+              rightFieldCallback: () => print("Right"),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _PictureAndPrice extends StatelessWidget {
+  final String picUrl;
+  final int dishPrice;
+
+  const _PictureAndPrice({@required this.picUrl, @required this.dishPrice});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        width: double.infinity,
+        height: 281.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: CachedNetworkImageProvider(picUrl),
+          ),
+        ),
+      ),
+      Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.blueGrey[100], width: 0),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Цена", style: TextStyle(fontSize: 15)),
+            Text(
+              "${dishPrice} руб.",
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    ]);
+  }
+}
+
+class _ButtonBar extends StatelessWidget {
+  final void Function() leftFieldCallback;
+  final void Function() rightFieldCallback;
+
+  const _ButtonBar(
+      {@required this.leftFieldCallback, @required this.rightFieldCallback});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        RawMaterialButton(
+          onPressed: leftFieldCallback,
+          child: Text("Левая хуйня"),
+        ),
+        RawMaterialButton(
+          onPressed: rightFieldCallback,
+          child: Text("Правая хуйня"),
+          fillColor: Colors.blue,
+        ),
+      ],
     );
   }
 }
