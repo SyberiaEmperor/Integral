0c71fdb31074f4d535a1ce5cc176cb8fddfd015a
Sakorni <ssakorni@gmail.com>
2021-02-07 14:18:55 +0300

    More files

--- lib/blocs/auth_bloc/auth_bloc.dart
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -1,18 +1,57 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:integral/entities/auth_data.dart';
+import 'package:integral/entities/user.dart';
+import 'package:integral/entities/user_repository.dart';
+import 'package:integral/utils/exceptions/auth_exceptions.dart';
+import 'package:integral_admin/entities/auth_data.dart';
+import 'package:integral_admin/entities/user.dart';
+import 'package:integral_admin/entities/user_repository.dart';
+import 'package:integral_admin/models/authentification.dart';
+import 'package:integral_admin/models/user_data_repository.dart';
+import 'package:integral_admin/utils/auth_exceptions.dart';
 import 'package:meta/meta.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial());
+  AuthBloc(
+      UserDataRepository dataRepository, AuthentificationInterface loginService)
+      : _urp = dataRepository,
+        _loginService = loginService,
+        super(AuthMainState());
+
+  final UserDataRepository _urp;
+  final AuthentificationInterface _loginService;
 
   @override
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield AuthInProgressState();
+    if (event is AuthFirstCheckEvent) {
+      AuthData data = await _urp.getData();
+      if (data != null) {
+        yield await logIn(data);
+      }
+    }
+    if (event is AuthLogInEvent) {
+      yield await logIn(event.data);
+    }
+    yield AuthMainState();
+  }
+
+  Future<AuthState> logIn(AuthData data) async {
+    try {
+      User user = await _loginService.logIn(data);
+      UserRepository.setUser(user);
+      return AuthLoggedInState();
+    } on AuthException catch (exception) {
+      return AuthErrorState(exception.message);
+    } on Exception {
+      rethrow;
+    }
   }
 }
--- lib/blocs/auth_bloc/auth_state.dart
+++ lib/blocs/auth_bloc/auth_state.dart
@@ -3,4 +3,14 @@ part of 'auth_bloc.dart';
 @immutable
 abstract class AuthState {}
 
-class AuthInitial extends AuthState {}
+class AuthMainState extends AuthState {}
+
+class AuthInProgressState extends AuthState {}
+
+class AuthErrorState extends AuthState {
+  final String caption;
+
+  AuthErrorState(this.caption);
+}
+
+class AuthLoggedInState extends AuthState {}
