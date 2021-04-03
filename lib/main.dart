e332e6704ba3318fc26b6ccdde5c5e3d3f8ca138
Sakorni <ssakorni@gmail.com>
2021-04-03 15:55:48 +0300

    Fixed some BLoC logic in AuthBloc

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class MyApp extends StatelessWidget {
           create: (context) => AuthBloc(
             SpDataRepository(),
             DioAuthenticator(),
-          )..add(AuthFirstCheckEvent()),
+          ),
           child: PhonePage(),
         );
       }),
