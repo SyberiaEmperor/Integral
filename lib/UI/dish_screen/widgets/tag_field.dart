5f79deea04b94d14e743663fc1d9f8fbb2937b3a
SyberiaImperor <Al.B.danilenko@gmail.com>
2021-05-15 18:53:34 +0300

    увеличены тэги категорий

--- lib/UI/dish_screen/widgets/tag_field.dart
+++ lib/UI/dish_screen/widgets/tag_field.dart
@@ -8,20 +8,25 @@ class TagField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 25.height,
-      padding: EdgeInsets.symmetric(horizontal: 18.width, vertical: 6.height),
-      decoration: BoxDecoration(
-        color: Theme.of(context).accentColor,
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Text(
-        tag,
-        style: Theme.of(context)
-            .accentTextTheme
-            .subtitle1
-            .copyWith(fontSize: 9.height),
-        textWidthBasis: TextWidthBasis.parent,
+    return Padding(
+      padding: EdgeInsets.only(right: 8.0.width, bottom: 8.height),
+      child: Container(
+        alignment: Alignment.center,
+        height: 35.height,
+        width: 125.width,
+        padding: EdgeInsets.symmetric(horizontal: 18.width, vertical: 6.height),
+        decoration: BoxDecoration(
+          color: Theme.of(context).accentColor,
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Text(
+          tag,
+          style: Theme.of(context)
+              .accentTextTheme
+              .subtitle1
+              .copyWith(fontSize: 11.height),
+          textWidthBasis: TextWidthBasis.parent,
+        ),
       ),
     );
   }
